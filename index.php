8fedc0ca8574fa02eb86c10a9158ba9042f01dd7
dattrong <52957785+dattrong@users.noreply.github.com>
2019-09-02 12:45:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -111,12 +111,12 @@ $result = queryMysql($query);
             <div class="header">
                 
                 <div class="nav">
-                    <ul class="w3-bar w3-black">
+                    <ul class="w3-bar w3-pink">
                         <center><li><a href="./index.php">Home</a></li></center>
-                        <li><a href="#Princess" class="w3-bar-item w3-hover-green">Princess</a></li>
-                        <li><a href="#BlackBaby" class="w3-bar-item w3-hover-green">BlackBaby</a></li>
-                        <li><a href="#Mermaid" class="w3-bar-item w3-hover-green">Mermaid</a></li>
-                        <center><li><a href="./header.php" class="w3-bar-item w3-hover-green" style="margin-left: 50%">Admin</a></li></center>
+                        <li><a href="#Princess" class="w3-bar-item w3-hover-white">Princess</a></li>
+                        <li><a href="#BlackBaby" class="w3-bar-item w3-hover-white">BlackBaby</a></li>
+                        <li><a href="#Mermaid" class="w3-bar-item w3-hover-white">Mermaid</a></li>
+                        <center><li><a href="./header.php" class="w3-bar-item w3-hover-white" style="margin-left: 80%">Admin</a></li></center>
 
                     </ul>
 
